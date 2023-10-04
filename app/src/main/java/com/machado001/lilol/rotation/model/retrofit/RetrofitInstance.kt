13f8cbc6035975b7c95be3458f79053fbfe2aaee
machado001 <machadowg@gmail.com>
2023-10-03 22:55:03 -0300

    fix: vazei a api kkkkkkkkkkkkkkkkkkkkkkk
    thank god it will expire soon

--- app/src/main/java/com/machado001/lilol/rotation/model/retrofit/RetrofitInstance.kt
+++ app/src/main/java/com/machado001/lilol/rotation/model/retrofit/RetrofitInstance.kt
@@ -6,8 +6,6 @@ import retrofit2.create
 
 object RetrofitInstance {
 
-    const val API_KEY = "RGAPI-63dcff38-573f-41a0-b085-2ecdea47a375"
-
     val api: RotationRemoteDataSource by lazy {
         Retrofit.Builder()
             .baseUrl("https://br1.api.riotgames.com")
