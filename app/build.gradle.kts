917a8134be1e77018b44469786c11a24feeb1b80
machado001 <machadowg@gmail.com>
2025-01-18 13:23:23 -0300

    upgrade gradle version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.machado001.lilol"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0.1"
+        versionCode = 10
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
